55118e6e7061de1d715a3d7632d3590839b5e4ab
Davi Gonçalves <blizzardavi@hotmail.com>
2017-10-02 19:19:48 -0300

    criação da tela de interesses e de sugestão

--- src/pages/interesse/interesse.ts
+++ src/pages/interesse/interesse.ts
@@ -6,9 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'interesse.html'
 })
 export class InteressePage {
-
+  items;
   constructor(public navCtrl: NavController) {
-
+    this.initializeItems();    
   }
+  initializeItems() {
+    this.items = [
+      'Outback',
+      'Calaf',
+      'Garota Carioca',
+      'Divina Lounge',
+      'Capela'
+    ];
+  }
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
 
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  
 }
